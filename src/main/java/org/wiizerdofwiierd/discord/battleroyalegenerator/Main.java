105687a820009f870dde9bdf31380f3dd177594c
wiizerdofwiierd <imtoby@comcast.net>
2019-02-26 03:52:24 -0800

    Increase version number

--- src/main/java/org/wiizerdofwiierd/discord/battleroyalegenerator/Main.java
+++ src/main/java/org/wiizerdofwiierd/discord/battleroyalegenerator/Main.java
@@ -19,7 +19,7 @@ import java.io.*;
 
 public class Main{
 	
-	public static final String VERSION = "1.3.1";
+	public static final String VERSION = "1.4";
 	
 	public static final File TOKEN_FILE = new File(System.getProperty("user.dir") + File.separator + "token.txt");
 	
