24e3038401b368c2c0a6951f6dc33169b002234f
wiizerdofwiierd <imtoby@comcast.net>
2019-03-04 17:17:13 -0800

    Whoops forgot a file

---
+++ src/main/java/org/wiizerdofwiierd/discord/battleroyalegenerator/game/event/EventContext.java
@@ -0,0 +1,28 @@
+package org.wiizerdofwiierd.discord.battleroyalegenerator.game.event;
+
+public enum EventContext{
+
+	BLOODBATH("Bloodbath"),
+	DAY("Day"),
+	NIGHT("Night"),
+	FEAST("Feast"),
+	ARENA("Arena");
+
+	private String name;
+
+	EventContext(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+
+	public String getTag(){
+		return this.name.toLowerCase();
+	}
+
+	public String getTagFatal(){
+		return getTag() + "fatal";
+	}
+}
